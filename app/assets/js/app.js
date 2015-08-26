e12dd9218c317cf039dd87124e7d35f117f8f5b6
sandeepgudipudi <sandeepgudipudi@users.noreply.github.com>
2015-08-26 15:49:26 -0500

    Responsive landing page first check-in

---
+++ app/assets/js/app.js
@@ -0,0 +1,27 @@
+/*
+ Cufon font repleactment code
+ * */
+Cufon.replace('.logo h1', {fontFamily: 'Inspiration'});
+
+Cufon.replace('.logo h2', {fontFamily: 'Museo Slab 100'});
+
+Cufon.replace('.logo h2 span', {fontFamily: 'Abraham Lincoln'});
+
+Cufon.replace('p.big_text, p.small_text', {fontFamily: 'Museo Slab 100'});
+
+Cufon.replace('p.big_text strong, p.small_text strong', {fontFamily: 'Museo Slab 700'});
+
+
+/*
+ ON WINDOW RESIZE
+ * */
+jQuery(window).bind('resize', (function() {
+  var timer;
+  return function() {
+    clearTimeout(timer);
+    //refresh cufon fonts with new font-size in css
+    timer = setTimeout(function() {
+      Cufon.refresh();
+    }, 50); 
+  };
+})());
