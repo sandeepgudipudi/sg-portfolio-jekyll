65e142abef48d5a0584a49a4d680334757cb6ace
sandeepgudipudi <samdipg@gmail.com>
2015-11-03 20:44:39 -0600

    basic default layout with menu styles

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,13 +3,13 @@
  * */
 Cufon.replace('.logo h1', {fontFamily: 'Inspiration'});
 
-Cufon.replace('.logo h2', {fontFamily: 'Museo Slab 100'});
+/*Cufon.replace('.logo h2', {fontFamily: 'Museo Slab 100'});
 
 Cufon.replace('.logo h2 span', {fontFamily: 'Abraham Lincoln'});
 
 Cufon.replace('p.big_text, p.small_text', {fontFamily: 'Museo Slab 100'});
 
-Cufon.replace('p.big_text strong, p.small_text strong', {fontFamily: 'Museo Slab 700'});
+Cufon.replace('p.big_text strong, p.small_text strong', {fontFamily: 'Museo Slab 700'});*/
 
 
 /*
